da87e32cc73516adff683d223b023f84cba937f2
Olivera Leonardo <olezdev@gmail.com>
2024-09-04 18:46:30 -0300

    remove Skill entity

--- Backend/JuniorHub.Persistence/Data/JuniorHubContext.cs
+++ Backend/JuniorHub.Persistence/Data/JuniorHubContext.cs
@@ -4,27 +4,26 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
-namespace JuniorHub.Persistence.Data
+namespace JuniorHub.Persistence.Data;
+
+public class JuniorHubContext : IdentityDbContext<User, IdentityRole<int>, int>
 {
-    internal class JuniorHubContext : IdentityDbContext<User, IdentityRole<int>, int>
+    public JuniorHubContext(DbContextOptions<JuniorHubContext> options) : base(options)
     {
-        public JuniorHubContext(DbContextOptions<JuniorHubContext> options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<Employer> Employers { get; set; }
-        public DbSet<Freelancer> Freelancers { get; set; }
-        public DbSet<Link> Links { get; set; }
-        public DbSet<Offer> Offers { get; set; }
-        public DbSet<Application> Applications { get; set; }
-        public DbSet<Skill> Skills { get; set; }
-        public DbSet<Technology> Technologies { get; set; }
-        public DbSet<Valoration> Valorations { get; set; }
+    public DbSet<Employer> Employers { get; set; }
+    public DbSet<Freelancer> Freelancers { get; set; }
+    public DbSet<Link> Links { get; set; }
+    public DbSet<Offer> Offers { get; set; }
+    public DbSet<Application> Applications { get; set; }
+    public DbSet<Technology> Technologies { get; set; }
+    public DbSet<Valoration> Valorations { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        Seeding.Seed.IntialSeed(builder);
     }
 }
