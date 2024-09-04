f3bd5bea1f81c187730aa835c92027f071630cb4
abelmv <mvabel23@gmail.com>
2024-09-03 23:06:34 -0300

    add EntityFrameworkCore.designbecasuse it is required to add migrations. Add an enum Role and set up seedData in dbContext to initialize the roles

---
+++ Backend/JuniorHub.Domain/Utilities/RoleExtensions.cs
@@ -0,0 +1,28 @@
+﻿using JuniorHub.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuniorHub.Domain.Utilities
+{
+    public static class RoleExtensions
+    {
+        public static string ToStringEnum(this Role role)
+        {
+            switch(role)
+            {
+                case Role.Employer:
+                
+                    return "Employer";
+                case Role.Freelancer:
+                    return "Freelancer";
+                case Role.Admin:
+                    return "Admin";
+                default:
+                    return role.ToString();
+            }
+        }
+    }
+}
---
+++ Backend/JuniorHub.Persistence/Configuration/IdentityRoleInitializer.cs
@@ -0,0 +1,20 @@
+﻿using JuniorHub.Domain.Enums;
+using JuniorHub.Domain.Utilities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JuniorHub.Persistence.Configuration
+{
+    internal class IdentityRoleInitializer : IEntityTypeConfiguration<IdentityRole<int>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole<int>> builder)
+        {
+            builder.HasData(
+           new IdentityRole<int> { Id = 1, Name = Role.Admin.ToStringEnum(), NormalizedName = Role.Admin.ToStringEnum().ToUpper() },
+           new IdentityRole<int> { Id = 2, Name = Role.Freelancer.ToStringEnum(), NormalizedName = Role.Freelancer.ToStringEnum().ToUpper() },
+           new IdentityRole<int> { Id = 3, Name = Role.Employer.ToStringEnum(), NormalizedName = Role.Employer.ToStringEnum().ToUpper() }
+             );
+        }
+    }
+}
