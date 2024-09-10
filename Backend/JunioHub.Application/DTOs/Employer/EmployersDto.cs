0a01e8a0a5798c52e48abfd05565bad20292e253
Olivera Leonardo <olezdev@gmail.com>
2024-09-10 12:56:47 -0300

    Refactor(employer): DTOs and update using directives

--- Backend/JunioHub.Application/DTOs/Employer/EmployersDto.cs
+++ Backend/JunioHub.Application/DTOs/Employer/EmployersDto.cs
@@ -1,17 +1,18 @@
 using JuniorHub.Domain.Enums;
 using JuniorHub.Domain.Entities;
+using JunioHub.Application.DTOs.Valoration;
 
-namespace JunioHub.Application.DTOs.Employer
+namespace JunioHub.Application.DTOs.Employer;
+
+public class EmployersDto
 {
-    public class EmployersDto
-    {
-         public int Id { get; set; }
-        public ValorationEnum Valoration { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; } = null!;
-        public string UserName { get; set; } = null!; 
-        public string UserLastName { get; set; } = null!; 
-        public ICollection<Offer> Offers { get; set; } = null!;
-        public ICollection<Valoration> Valorations { get; set; } = null!;
-    }
+    public int Id { get; set; }
+    public ValorationEnum Valoration { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string UserLastName { get; set; } = null!;
+    public ICollection<Offer> Offers { get; set; } = null!;
+    public IEnumerable<ValorationDto> Valorations { get; set; } = Enumerable.Empty<ValorationDto>();
+
 }
