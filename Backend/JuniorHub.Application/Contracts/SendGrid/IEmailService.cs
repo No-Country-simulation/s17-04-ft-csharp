dd65a7aa7cc713997a52791e6a100909b316019d
abelmv <mvabel23@gmail.com>
2024-09-14 18:58:05 -0300

    Add IEmailService interface and add section in appSettings for apikey of SendGrid

---
+++ Backend/JuniorHub.Application/Contracts/SendGrid/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using JuniorHub.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuniorHub.Application.Contracts.SendGrid
+{
+    public interface IEmailService
+    {
+        Task<BaseResponse<bool>> SendGridEmailAsync(string emailTo, string emailSubject, string emailBody);
+    }
+}
