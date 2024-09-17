f43787ec296b1e61eab646259de043b1f8eddd35
Olivera Leonardo <olezdev@gmail.com>
2024-09-17 12:19:56 -0300

    feat: Added list of Links to ApplicationsByOffer

--- Backend/JuniorHub.Application/DTOs/Link/LinkAddDto.cs
+++ Backend/JuniorHub.Application/DTOs/Link/LinkAddDto.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace JuniorHub.Application.DTOs.Link;
 
-namespace JuniorHub.Application.DTOs.Link
+public class LinkAddDto
 {
-    public class LinkAddDto
-    {
-        public string Name { get; set; }
-        public string Url { get; set; }
-    }
+    public string Name { get; set; }
+    public string Url { get; set; }
 }
